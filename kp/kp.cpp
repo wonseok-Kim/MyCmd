8f3f6013af26a372ce43c7e90b042271e1d0c0f9
DESKTOP-8BTVEQ0\wonse <wonseok.dev@gmail.com>
2022-10-13 17:44:40 +0900

    kp [파일이름] 파일 이름 대소문자 처리

--- kp/kp.cpp
+++ kp/kp.cpp
@@ -15,6 +15,23 @@ namespace std
 #endif // UNICODE
 }
 
+auto ToLowerString(const std::tstring& str) -> std::tstring
+{
+    std::tstring result{};
+    result.reserve(str.length());
+
+    for (auto ch : str)
+    {        
+        if (_T('A') <= ch && ch < _T('Z'))
+        {
+            ch += _T('a') - _T('A');
+        }
+        result.push_back(ch);
+    }
+    
+    return result;
+}
+
 auto _tmain(int32_t argc, LPTSTR argv[]) -> int32_t
 {
     _tsetlocale(LC_ALL, _T("Korean"));
@@ -25,8 +42,8 @@ auto _tmain(int32_t argc, LPTSTR argv[]) -> int32_t
         return 0;
     }
 
-    std::tstring targetName{ argv[1] };
-
+    std::tstring targetName{ ToLowerString(argv[1]) };
+    
     HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE)
     {
@@ -43,8 +60,8 @@ auto _tmain(int32_t argc, LPTSTR argv[]) -> int32_t
         goto CLOSE_AND_EXIT;
     }
     do
-    {
-        if (targetName == pe32.szExeFile)
+    {        
+        if (targetName == ToLowerString(pe32.szExeFile))
         {
             HANDLE hToKill = OpenProcess(PROCESS_TERMINATE, FALSE, pe32.th32ProcessID);
             TerminateProcess(hToKill, 1);
